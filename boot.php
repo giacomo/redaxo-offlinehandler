ca776dc0fc23b833927604387e373c537f68cdd1
giacomo <info@ready24it.eu>
2021-11-19 14:56:31 +0100

    first commit

---
+++ boot.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * offlinehandler Addon.
+ *
+ * @author Giacomo Barbalinardo
+ *
+ * @var rex_addon
+ */
+if (rex::isBackend()) {
+} else {
+    rex_extension::register('PACKAGES_INCLUDED', function () {
+        if (rex_article::getCurrent() instanceof rex_article && rex_article::getCurrent()->getValue('status') == 0 && !rex_backend_login::hasSession()) {
+
+            if (rex_article::getCurrent(rex_clang::getStartId())->getValue('status') == 0) {
+                if (rex_article::getCurrentId() === rex_article::getNotfoundArticleId()) {
+                    return;
+                }
+
+                rex_redirect(rex_article::getNotfoundArticleId(), rex_clang::getCurrentId());
+            }
+        }
+    }, rex_extension::LATE);
+}
